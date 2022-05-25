831ebb120ebc9425a9deb8e928c70d62624dd486
eqpoqpe <eqpoqpe@gmail.com>
2022-05-25 20:37:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-wgdsif
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-wgdsif)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// string.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'').trim()
+
+const proot = document.querySelector("#page-root");
+
+// data to mapping tree
+const MAPPING_DIRR_RERENDER = {
+};
