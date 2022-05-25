48ee4e90bbf8dc02d06a4bbc05aeebf5239c6bd7
eqpoqpe <eqpoqpe@gmail.com>
2022-05-25 21:21:23 +0800

    upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
-// string.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'').trim()
-
 const proot = document.querySelector("#page-root");
 
+const __SLICE__ = false;
+
 // data to mapping tree
+// disabled properties
 const MAPPING_DIRR_RERENDER = {
+  MAPPING_RENDER_TREE: [
+    {
+      _$: 0,
+      tag: "div",
+      content: [
+        22,
+        {
+          _$: 0,
+          tag: "div",
+          content: [
+            {
+              _$: 0,
+              tag: "div",
+              content: [
+                0
+              ]
+            }
+          ]
+        },
+        {
+          _$: 0,
+          tag: "div",
+          content: []
+        }
+      ]
+    }
+  ]
 };
+
+// first page render
+function le_render() {}
+
+// supported diff-commands render
+function render() {}
